3a039bcc56453068a97c08160a5e4641f6457d31
garypy <imblues@126.com>
2016-04-11 00:33:05 +0800

    Create a username filter in contract admin page, then it can list by the special user. The second one is add search box in the contract admin page. These two part is very important for administrator to get registered user's contract.

--- contract/admin.py
+++ contract/admin.py
@@ -2,9 +2,19 @@ from django.contrib import admin
 from contract import models 
 
 class UserContractAdmin(admin.ModelAdmin):
-    list_display = ('user', 'name', 'backlog', 'content')
+    #list_display = ('user', 'name', 'backlog', 'content')
+    #list_filter = ('user', 'type')
+    list_filter = ('user', )
+    raw_id_admin = ('user')
+    search_fields = ['user__username', 'type', 'name']
     #show fields in the edit mode
     #fields = (('name', 'backlog'), 'content')
+
+"""
+class ContractOpts(admin.ModelAdmin):
+    list_filter = ('user')
+    raw_id_admin = ('user')
+"""
 
 admin.site.register(models.UserProfile)
 admin.site.register(models.UserContract, UserContractAdmin)
