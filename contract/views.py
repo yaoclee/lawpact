853f9acd07c5eca352026b6778e54ff68b127b18
garypy <imblues@126.com>
2016-04-30 09:28:47 +0800

    Complete login functionality

--- contract/views.py
+++ contract/views.py
@@ -11,28 +11,24 @@ def index(request):
 
 
 def user_login(request):
-    #context = RequestContext(request)
+    context = RequestContext(request)
+    error_msg = ""
     if request.method == 'POST':
         print('user authentic')
-        return render(request, 'contract/index.html')
-    else:
-        return render(request, 'contract/login.html')
-
-        """
         username = request.POST['username']
         password = request.POST['password']
         
         user = authenticate(username=username, password=password)
         if user is not None:
             if user.is_active:
-                login(request, user)
-                return render(request, 'contract/index.html')
-                #return HttpResponseRedirect('/')
+                login(request, user) #produce session
+                #return render(request, 'contract/index.html')
+                return HttpResponseRedirect('/')
             else:
-                return HttpResponse("User account is disabled!")
+                error_msg = "User was not active!"
+                #return render(request, "User account is disabled!")
         else:
             print "Invalid login details: {0}, {1}".format(username, password)
-            HttpResponse("Invalid detailed provided.")
-    else:
-        return render_to_response('contract/login.html', {}, context)
-        """
+            error_msg = "Wrong username or password!"
+            #render(request, "Invalid detailed provided.")
+    return render(request, 'contract/login.html', {'err_msg' : error_msg})
